273c93a357a7639dd2c29325626d10e8ab53a18b
Mohamed Abd-Elhamed Ahmed <73020992+Mohamedabdelhamed382@users.noreply.github.com>
2022-05-07 02:03:52 +0200

    Initial Commit

---
+++ SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by MOHAMED ABD ELHAMED AHMED on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
